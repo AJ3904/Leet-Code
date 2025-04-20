63eb6e50f264602119f3be4d2dde90e96f89cf5a
adlijohan <adlijohan5@gmail.com>
2025-04-20 12:57:35 -0400

    ReverseInteger

---
+++ ReverseInteger.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverse(self, x):
+        result = 0
+        sign = 1 if x > 0 else -1
+        x = x * sign
+        while x > 0:
+            result = result * 10 + (x % 10)
+            x = x // 10
+            if result > 2**31 - 1:
+                return 0
+        return result * sign
